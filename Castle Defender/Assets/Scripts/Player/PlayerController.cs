3843318245bf251c3aebc0a8a96bd33ac8b5a818
GishaTheDeveloper <spaceshhz@gmail.com>
2020-06-08 23:47:43 +0300

    Added Player Animations and Player Controller

---
+++ Castle Defender/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Attack Animation
+        animator.SetTrigger("Attack");
+    }
+}
--- Castle Defender/Assets/Scripts/Player/PlayerMovement.cs
+++ Castle Defender/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,17 +7,30 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
 
     Rigidbody2D rb;
+    Animator animator;
+
+    enum States
+    {
+        Idle,
+        Run
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
 
     void FixedUpdate()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
+        if (moveInput.x > 0)
+            TurnSprite(1f);
+        else if (moveInput.x < 0)
+            TurnSprite(-1f);
+
+        MovementAnimation();
         rb.velocity += new Vector2(HorizontalVel(), VerticalVel());
     }
 
@@ -43,4 +56,18 @@ public class PlayerMovement : MonoBehaviour
     {
         return x < WorldBounds.Instance.b_Right.position.x && x > WorldBounds.Instance.b_Left.position.x;
     }
+
+    void MovementAnimation()
+    {
+        if (Mathf.Abs(moveInput.x) > 0f || Mathf.Abs(moveInput.y) > 0f)
+            animator.SetInteger("State", (int)States.Run);
+        else
+            animator.SetInteger("State", (int)States.Idle);
+    }
+
+    void TurnSprite(float x)
+    {
+        transform.localScale = new Vector2(x, 1f);
+    }
+
 }
